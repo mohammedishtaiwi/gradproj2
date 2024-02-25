84400caba4500d79e7b8815b9200265003846185
unknown <slh>
2024-02-26 00:08:27 +0300

    test

--- lib/main.dart
+++ lib/main.dart
@@ -5,22 +5,20 @@ import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,);
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
-  
 }
-
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      debugShowCheckedModeBanner: false, 
+      debugShowCheckedModeBanner: false,
       title: 'Login App',
       initialRoute: Routes.login,
       onGenerateRoute: Routes.generateRoute,
     );
   }
 }
-
-
