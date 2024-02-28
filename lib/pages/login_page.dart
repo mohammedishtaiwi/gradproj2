ddd212d3ba16d5830e81c7460544b76ddc12d011
seamangooo <samaalzoubi01@gmail.com>
2024-02-28 14:05:22 +0300

    invalid user or pass

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,6 +1,5 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-
+import 'package:firebase_auth/firebase_auth.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -16,7 +15,7 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        automaticallyImplyLeading: false, 
+        automaticallyImplyLeading: false,
         title: const Center(child: Text('Login Page')),
       ),
       body: Center(
@@ -38,7 +37,7 @@ class _LoginPageState extends State<LoginPage> {
               const SizedBox(height: 20),
               ElevatedButton(
                 onPressed: () async {
-                  await _signInWithEmailAndPassword();
+                  await _signInWithEmailAndPassword(context);
                 },
                 child: const Text('Login'),
               ),
@@ -56,7 +55,7 @@ class _LoginPageState extends State<LoginPage> {
     );
   }
 
-  Future<void> _signInWithEmailAndPassword() async {
+  Future<void> _signInWithEmailAndPassword(BuildContext context) async {
     try {
       await _auth.signInWithEmailAndPassword(
         email: _emailController.text,
@@ -67,7 +66,34 @@ class _LoginPageState extends State<LoginPage> {
       Navigator.pushReplacementNamed(context, '/home');
     } catch (e) {
       print('Failed to sign in: $e');
-      // Handle login failure (show a snackbar, etc.)
+      // Handle login failure (show a popup window)
+      showModalBottomSheet(
+        context: context,
+        builder: (BuildContext context) {
+          return Container(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                const Text(
+                  'Error: Invalid username or password',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: const Text('OK'),
+                ),
+              ],
+            ),
+          );
+        },
+      );
     }
   }
 
@@ -84,7 +110,7 @@ class _LoginPageState extends State<LoginPage> {
         // User exists, navigate to the home page
         Navigator.pushReplacementNamed(context, '/home');
       } else {
-        // User doesn't exist, prompt them to sign up
+        // User doesn't exist, navigate to the sign-up page hon mu zabet
         Navigator.pushReplacementNamed(context, '/signup');
       }
     } catch (e) {
