93fa942daa0affacd3558e184bc7f8e82bf57309
Mohammed ishtaiwi <mohammedfunu@gmail.com>
2024-05-17 00:31:25 +0300

    fixes

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -6,7 +6,6 @@ import 'package:gradproj2/theme/theme_manager.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-// ignore: camel_case_types
 class home extends StatefulWidget {
   const home({Key? key}) : super(key: key);
 
@@ -14,7 +13,6 @@ class home extends StatefulWidget {
   State<home> createState() => _homeState();
 }
 
-// ignore: camel_case_types
 class _homeState extends State<home> with SingleTickerProviderStateMixin {
   late TabController _tabController;
 
@@ -48,70 +46,44 @@ class _homeState extends State<home> with SingleTickerProviderStateMixin {
       child: SafeArea(
         child: Scaffold(
           backgroundColor: notifire.backgroundallscreenColor,
-          //resizeToAvoidBottomInset: false,
           body: Column(
             children: [
-              Align(
-                alignment: Alignment.center,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 0),
-                  child: Row(
-                    children: [
-                      Container(
-                        color: Color.fromARGB(255, 114, 151, 172),
-                        width: 392.5,
-                        child: TabBar(
-                          controller: _tabController,
-                          indicator: const UnderlineTabIndicator(
-                              insets: EdgeInsets.symmetric(horizontal: 10),
-                              borderSide:
-                                  BorderSide(width: 2, color: Colors.white)),
-                          // unselectedLabelStyle: TextStyle(color: Colors.red),
-                          // unselectedLabelColor: TextStyle(color: Colors.red),
-                          labelPadding:
-                              const EdgeInsets.only(left: 15, right: 15),
-                          isScrollable: true,
-                          indicatorSize: TabBarIndicatorSize.label,
-                          indicatorColor: notifire.getdarkscolor,
-                          labelColor: notifire.getdarkscolor,
-                          dividerColor: Color.fromARGB(255, 114, 151, 172),
-                          tabAlignment: TabAlignment.center,
-                          tabs: [
-                            Tab(
-                              child: Text(
-                                "Sign in",
-                                style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 18,
-                                    fontFamily: 'Gilroy'),
-                              ),
-                            ),
-                            Tab(
-                              child: Text(
-                                "Sign up",
-                                style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 18,
-                                    fontFamily: 'Gilroy'),
-                              ),
-                            ),
-                          ],
+              Container(
+                color: Color.fromARGB(255, 114, 151, 172),
+                child: TabBar(
+                  controller: _tabController,
+                  indicator: const UnderlineTabIndicator(
+                    insets: EdgeInsets.symmetric(horizontal: 10),
+                    borderSide: BorderSide(width: 2, color: Colors.white),
+                  ),
+                  labelPadding: const EdgeInsets.only(left: 15, right: 15),
+                  isScrollable: false,
+                  indicatorSize: TabBarIndicatorSize.label,
+                  indicatorColor: notifire.getdarkscolor,
+                  labelColor: notifire.getdarkscolor,
+                  dividerColor: const Color.fromARGB(255, 114, 151, 172),
+                  tabs: const [
+                    Tab(
+                      child: Text(
+                        "Sign in",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                          fontFamily: 'Gilroy',
                         ),
                       ),
-                      // Container(
-                      //   //height: 8,
-                      //   decoration: BoxDecoration(
-                      //       gradient: LinearGradient(
-                      //     begin: Alignment.topRight,
-                      //     end: Alignment.bottomLeft,
-                      //     colors: [
-                      //       Colors.blue.shade100,
-                      //       Colors.white12,
-                      //     ],
-                      //   )),
-                      // ),
-                    ],
-                  ),
+                    ),
+                    Tab(
+                      child: Text(
+                        "Sign up",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                          fontFamily: 'Gilroy',
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
               Expanded(
